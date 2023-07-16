0420dabf92329ebac7c56567028051e68afd5512
Ahsanur-42 <mdahsanurrahaman2456@gmail.com>
2023-07-16 13:18:22 +0600

    secondclass addedasss

---
+++ secondclass.java
@@ -0,0 +1,10 @@
+public class secondclass{
+	public static void main(String []arg)
+	{
+		System.out.println("Hello word");
+		for(int i=0;i<arg.length;i++)
+		{
+			System.out.println(arg[i]+" ");
+		}
+	}
+}
