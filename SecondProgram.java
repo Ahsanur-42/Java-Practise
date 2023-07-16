4d4abcb2bf88666251fbba286bc0aa4136d53b87
Ahsanur-42 <mdahsanurrahaman2456@gmail.com>
2023-07-16 13:18:43 +0600

    fasfd

---
+++ SecondProgram.java
@@ -0,0 +1,10 @@
+public class SecondProgram{
+	public static void main(String []arg)
+	{
+		System.out.println("Hello World");
+		for(int i=0;i<arg.length;i++)
+		{
+			System.out.println(arg[i]+" ");
+		}
+	}
+}
