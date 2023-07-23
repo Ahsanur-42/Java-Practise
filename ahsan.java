147fc0a4cfee25baa75a5c0bb0c4bcad0198db73
Ahsanur-42 <mdahsanurrahaman2456@gmail.com>
2023-07-23 13:27:52 +0600

    Factorial added

--- ahsan.java
+++ ahsan.java
@@ -4,6 +4,7 @@ public class ahsan {
     public void print_a()
     {
         Scanner input = new Scanner(System.in);
+        System.out.print("The value of n: ");
         a = input.nextInt();
         System.out.println("The value of a " +a);
     }
