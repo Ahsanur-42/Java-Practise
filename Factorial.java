18803fdb4ff37964c9231d6ce1c1fe65bb0afeb4
Ahsanur-42 <mdahsanurrahaman2456@gmail.com>
2023-07-23 12:52:05 +0600

    Factorial added

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,15 @@
-public class Practise1 {
+import java.util.Scanner;
+public class Factorial{
     public static void main(String []arg)
     {
-        System.out.println("Emni e code likhlam!");
+        int num;
+        long fact=1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        num=input.nextInt();
+        for(int i=num;i>0;i--)
+            fact*=i;
+        System.out.println("Factorial of "+num+" is "+fact);
+
     }
 }
