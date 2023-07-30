df56a23a42819f33e4b4b2fc516525e0fae711ea
Ahsanur-42 <mdahsanurrahaman2456@gmail.com>
2023-07-30 12:23:40 +0600

    Array Added

---
+++ Array.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Array{
+    public static void main(String []arg)
+    {
+        int arr[] = new int[10];
+        int i;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter value of Array: ");
+        for(i=0;i<10;i++)
+        arr[i] = input.nextInt();
+
+        for(i=0;i<10;i++)
+        System.out.println("The Output is: "+arr[i]);
+
+        System.out.print(" ");
+        System.out.print("Enter element for searching: ");
+        int SearchElement = input.nextInt();
+        for(i=0;i<10;i++)
+        if(SearchElement==arr[i])
+        {
+            System.out.println("Found at position!");
+            break;
+        }
+        if(i==10)
+        System.out.println("Not Found!");
+
+
+
+
+    }
+    
+}
