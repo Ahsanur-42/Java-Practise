a94dd17d526f8caf810ed2e955664337056fe794
Ahsanur-42 <mdahsanurrahaman2456@gmail.com>
2023-08-10 23:10:06 +0600

    Dog Java

---
+++ Dog.java
@@ -0,0 +1,19 @@
+public class Dog {
+    String breed;
+    String size;
+    int age;
+    String color;
+    public String getInfo()
+    {
+        return("Breed is:"+breed+" Size is:"+size+" Age is:"+age+"  Color is:"+color+"");
+    }
+    public static void main(String []arg)
+    {
+        Dog maltese = new Dog();
+        maltese.breed = "Maltese";
+        maltese.size = "Small";
+        maltese.age = 2;
+        maltese.color = "White";
+        System.out.println(maltese.getInfo());
+        }
+}
